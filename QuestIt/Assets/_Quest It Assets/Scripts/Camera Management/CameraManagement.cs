f03a3d274e6a620d12b2db358b7429747f3cb144
SricharanYamsani <43514338+SricharanYamsani@users.noreply.github.com>
2019-11-22 20:47:09 +0530

    PUN 2 pulgin added
    Added PUN 2 and folder restructure

---
+++ QuestIt/Assets/_Quest It Assets/Scripts/Camera Management/CameraManagement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagement : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 positionOffset;
+    
+    public GameObject lookat;
+
+    private bool moveCamera = false;
+
+    private void Awake ( )
+    {
+        // We will find the object later. todo
+        if ( lookat )
+        {
+            moveCamera = true;
+        }
+    }
+    private void LateUpdate ( )
+    {
+        if ( moveCamera )
+        {
+            transform.localPosition = new Vector3 ( lookat.transform.position.x , lookat.transform.position.y + positionOffset.y , lookat.transform.position.z - positionOffset.x );
+        }
+    }
+
+    void PanCamera(float deltaAngle)
+    {
+    }
+}
