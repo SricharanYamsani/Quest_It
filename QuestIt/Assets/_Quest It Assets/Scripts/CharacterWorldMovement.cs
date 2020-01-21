9062ba2c4a5e354aa1ea328ae11ccfafc49fd1c6
rishabh2011 <merishabhchoudhary@gmail.com>
2020-01-22 01:28:19 +0530

    -Added new World scene -Added navmesh based point-click player movement -Added cinemachine player camera

---
+++ QuestIt/Assets/_Quest It Assets/Scripts/CharacterWorldMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterWorldMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToCursor();
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;        
+            }
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 localVelocity = transform.InverseTransformDirection(agent.velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("ForwardSpeed", speed);
+    }
+}
