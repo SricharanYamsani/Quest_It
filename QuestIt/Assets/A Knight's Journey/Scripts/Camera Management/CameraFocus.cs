d7a1ea00a225d88f40b1aa70ba1cfb9f68c8cc61
ILLUTE <illute@icoud.com>
2020-02-01 01:35:38 +0530

    Updated Scene with a new Camera Movement.

--- QuestIt/Assets/A Knight's Journey/Scripts/Camera Management/CameraFocus.cs
+++ QuestIt/Assets/A Knight's Journey/Scripts/Camera Management/CameraFocus.cs
@@ -12,6 +12,12 @@ public class CameraFocus : MonoBehaviour
 
     private Transform lookAtObject, prevLookAtObject = null;
 
+    private bool isMoving = false;
+
+    private float journeyLength, distanceCovered, startTime;
+
+    public float speed = 1.5f;
+
     private void Awake()
     {
         BattleManager.Instance.TurnStart += GetLookAt;
@@ -21,11 +27,33 @@ public class CameraFocus : MonoBehaviour
     {
         lookAtObject = player.LookAt;
 
-        if (lookAtObject)
-        {
-            this.transform.DORotate(new Vector3(0,30,0), 0.5f);
+        prevLookAtObject = transform;
 
-            this.transform.DOMove(lookAtObject.localPosition - offset, 0.5f);
+        if (prevLookAtObject != lookAtObject)
+        {
+            startTime = Time.time;
+
+            journeyLength = Vector3.Distance(prevLookAtObject.position, lookAtObject.position);
+
+            isMoving = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (isMoving)
+        {
+            distanceCovered = (Time.time - startTime) * speed;
+
+            float fracJourney = distanceCovered / journeyLength;
+
+            float singleStep = speed * Time.deltaTime;
+
+            transform.position = Vector3.Lerp(prevLookAtObject.position, offset.magnitude * lookAtObject.position, fracJourney);
+
+            Vector3 newDirection = Vector3.RotateTowards(prevLookAtObject.position, lookAtObject.position, singleStep, 0.0f);
+
+            transform.rotation = Quaternion.LookRotation(-1 * newDirection);
         }
     }
 
