c0875b13b9fa31435c1e2afea0ae28ad64141be1
SricharanYamsani <43514338+SricharanYamsani@users.noreply.github.com>
2020-04-26 23:59:48 +0530

    [WIP] Inventory and loot management basic scripts and prefabs.

---
+++ QuestIt/Assets/A Knight's Journey/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInventory
+{
+    Dictionary<Consumables, int> playerConsumables;
+    int coins;
+
+    public PlayerInventory()
+    {
+        playerConsumables = new Dictionary<Consumables, int>();
+        coins = 0;
+
+        FillRandomConsumables();
+    }
+    public int Coins
+    {
+        get;
+    }
+
+    public void AddCoins(int value)
+    {
+        coins += value;
+    }
+
+    public Dictionary<Consumables,int> GetPlayerConsumables()
+    {
+        return playerConsumables;
+    }
+
+    public int GetConsumableQuantity(Consumables consumables)
+    {
+        if(playerConsumables.ContainsKey(consumables))
+        {
+            return playerConsumables[consumables];
+        }
+        return 0;
+    }
+
+    public void AddConsumable(Consumables consumables, int value)
+    {
+        if(playerConsumables.ContainsKey(consumables))
+        {
+            playerConsumables[consumables] += value;
+        }
+        else
+        {
+            playerConsumables.Add(consumables, value);
+        }
+    }
+
+    void FillRandomConsumables()
+    {
+        List<Consumables> availableConsumableList = GameManager.allConsumables;
+
+        if (availableConsumableList != null)
+        {
+            for (int i = 0; i < availableConsumableList.Count; i++)
+            {
+                if (Random.value < 0.6) continue;
+
+                AddConsumable(availableConsumableList[i], Random.Range(0, 3));
+            }
+        }
+    }
+}
---
+++ QuestIt/Assets/A Knight's Journey/Scripts/UI/InventoryManagement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManagement : MonoBehaviour
+{
+    public Transform contentTransform;
+    GridLayout gridLayoutComponent;
+
+    public InventoryItem inventoryItemPrefab;
+
+    public void Init(PlayerInventory playerInventory)
+    {
+        Dictionary<Consumables, int> playerConsumables = playerInventory.GetPlayerConsumables();
+
+        foreach (Consumables consumable in playerConsumables.Keys)
+        {
+            InventoryItem item = Instantiate(inventoryItemPrefab, contentTransform);
+            item.Init(consumable.ToString(), playerConsumables[consumable]);
+        }
+    }
+}
